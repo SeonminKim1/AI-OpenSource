a5e664d9370150c5773285a5ab3d0c9f91128523
SeonminKim1 <yubi5050@hansung.ac.kr>
2021-03-04 15:45:08 +0900

    yolov5 detect gun

---
+++ yolov5/data/gun_data/gundata_file_classification.py
@@ -0,0 +1,29 @@
+
+from glob import glob
+
+# get image list
+img_list = glob('images/*.jpg')
+
+from sklearn.model_selection import train_test_split
+
+train_img_list, val_img_list = train_test_split(img_list, test_size=0.2, random_state=7)
+print(len(train_img_list), len(val_img_list))
+
+with open('train.txt', 'w') as f:
+    f.write('\ndata/gun_data/'.join(train_img_list)+'\n')
+
+with open('val.txt', 'w') as f:
+    f.write('\ndata/gun_data/'.join(val_img_list)+'\n')
+
+import yaml
+
+with open('data.yaml', 'r') as f:
+    data = yaml.load(f)
+
+data['train'] = 'data/gun_data/train.txt'
+data['val'] = 'data/gun_data/val.txt'
+
+with open('data.yaml', 'w') as f:
+    yaml.dump(data, f)
+
+print(data)
